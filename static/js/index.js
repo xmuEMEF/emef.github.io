d51604db269d13a81f7be46c542fe3abee780006
MedalWill <42199368+medalwill@users.noreply.github.com>
2023-12-15 16:18:30 +0800

    interpolation

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-var INTERP_BASE = "./static/interpolation/stacked";
-var NUM_INTERP_FRAMES = 240;
+var INTERP_BASE = "./static/assets/interpolation";
+var NUM_INTERP_FRAMES = 31;
 
 var interp_images = [];
 function preloadInterpolationImages() {
